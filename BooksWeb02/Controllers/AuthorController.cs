f3d4ad925250ec17ef4dacd6abbff3dd68db72d5
Sachin <jerryon1027@gmail.com>
2023-08-23 14:50:12 +0530

    first commit

---
+++ BooksWeb02/Controllers/AuthorController.cs
@@ -0,0 +1,108 @@
+﻿using ConceptArchitect.BookManagement;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BooksWeb02.Controllers
+{
+    public class AuthorController:Controller
+    {
+        IAuthorService authorService;
+
+        public AuthorController(IAuthorService authors)
+        {
+            this.authorService = authors;
+        }
+
+        public async Task<ViewResult> Index()
+        {
+            var authors = await authorService.GetAllAuthors();
+
+            return View(authors);
+        }
+
+        public async Task<ViewResult> Details(string id)
+        {
+            var author = await authorService.GetAuthorById(id);
+
+            return View(author);
+        }
+
+
+
+        [HttpGet]
+        public ViewResult Add()
+        {
+            return View(new Author());
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Add(Author author)
+        {
+            await authorService.AddAuthor(author);
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public async Task<ViewResult> Edit(string id)
+        {
+            var author = await authorService.GetAuthorById(id);
+            return View(author); 
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Edit(Author author)
+        {
+            await authorService.UpdateAuthor(author);
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> Delete(string id)
+        {
+            await authorService.DeleteAuthor(id);
+            Index();
+            return RedirectToAction("Index");
+
+        }
+
+
+        public async Task<ActionResult> SaveV2(Author author)
+        {
+            await authorService.AddAuthor(author);
+
+            return RedirectToAction("Index");
+        }
+
+
+
+        public Author SaveV1(string id, string name, string bio, string email, string photourl, DateTime dob)
+        {
+            var author = new Author()
+            {
+               Id=id,
+               Name=name,
+               Biography=bio,
+               Email=email,
+               BirthDate=dob,
+               Photo=photourl
+            };
+
+            return author;
+        }
+
+        public Author SaveV0()
+        {
+            var author = new Author()
+            {
+                Id = Request.Form["id"],
+                Name = Request.Form["name"],
+                Biography = Request.Form["bio"],
+                Email = Request.Form["email"],
+                Photo = Request.Form["photourl"]
+            };
+
+            return author;
+        }
+    }
+}
---
+++ BooksWeb02/Controllers/BookController.cs
@@ -0,0 +1,73 @@
+﻿using ConceptArchitect.BookManagement;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BooksWeb02.Controllers
+{
+    public class BookController:Controller
+    {
+        IBookService bookService;
+
+        public BookController(IBookService bookService)
+        {
+            this.bookService = bookService;
+        }
+
+
+        public async Task<ViewResult> Index()
+        {
+            var books = await bookService.GetAllBooks();
+            return View(books);
+        }
+
+        public async Task<ViewResult> Details(string id)
+        {
+            var author = await bookService.GetBookById(id);
+            return View(author);
+        }
+
+        [HttpGet]
+        public ViewResult Add()
+        {
+            return View(new Book());
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Add(Book book)
+        {
+            await bookService.AddBook(book);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public async Task<ViewResult> Edit(string id)
+        {
+            var author = await bookService.GetBookById(id);
+            return View(author);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Edit(Book book)
+        {
+            await bookService.UpdateBook(book);
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> Delete(string id)
+        {
+            await bookService.DeleteBook(id);
+            Index();
+            return RedirectToAction("Index");
+            
+        }
+
+        //[HttpPost]
+        //public async Task<ActionResult> Delete()
+        //{
+            
+            
+        //}
+
+    }
+}
